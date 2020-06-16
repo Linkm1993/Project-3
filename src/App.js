aa53c29bcc3a4c8448598de00dd9f0b5d264e9bc
Cosplay Diver <kaydeekhing@gmail.com>
2020-06-15 21:42:21 -0400

    added Canvas component and a test slider in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Canvas from "./components/Canvas";
 
 function App() {
+
+  const [textPosition, setTextPosition] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas textPosition = {textPosition} image = "https://www.wallpaperflare.com/static/617/455/618/avatar-the-last-airbender-last-air-bender-wallpaper-preview.jpg" text = "We are going on a trip" />
+      <input type="range" id="points" name="points" min="0" max="100" value={textPosition} onChange = {(e) => setTextPosition(e.target.value)} />
+    
     </div>
+
+
   );
 }
 
