fa24608bb06c879ba11d6f5b15d6f8fbb5853083
Alex Taylor <altays633@gmail.com>
2020-06-17 12:39:30 -0400

    adding logic to iterate over entries stored in state

--- src/components/pages/TestPage3.js
+++ src/components/pages/TestPage3.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { useHistory } from "react-router-dom";
+import API from "../../utils/api"
+import {GalleryList, GalleryListItem} from "../GalleryList"
 
 function TestPage3()  {
     const history = useHistory();
-      
-    function handleClick () {
+    const [memes, setMemes] = useState([])
+    
+    const handleClick = event => {
+        event.preventDefault();
         history.push("/testpage2");
     }
 
-        return (
-            <div>
-                <p>This is my test text3 </p>
-                <button onClick={handleClick}>Switch to page 2</button>
-            </div>
-        );
+    // function to make a call to the database, return memes, and store in state
+    const handleMemes = event => {
+        event.preventDefault();
+        console.log("Handling Memes")
+        API.getMemes()
+            .then(res =>setMemes(res.data))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <p>This is my test text3 </p>
+            <button onClick={handleClick}>Switch to page 2</button>
+            <button onClick={handleMemes}>Load Memes</button>
+            {!memes.length ? ( 
+                <h1> No Memes to Display</h1>
+            ) : (
+                <GalleryList>
+                {memes.map(meme => {
+                    return (
+                        <GalleryListItem 
+                            href={meme.image}
+                            alt={meme.title}
+                            // id = {meme._id}
+                        />
+                        )
+                    })
+                })
+                </GalleryList>
+            )}
+        </div>
+    );
 }
 
 export default TestPage3;
