8ed0abae07ce88191a991446cdf0771bac53ea59
Regina Musovirova <musovirova@yahoo.com>
2020-06-17 20:38:11 -0400

    create welcome.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Wrapper from "../src/components/Wrapper";
-import TestPage1 from "../src/components/pages/TestPage1";
+import Welcome from "../src/components/pages/Welcome";
 import TestPage2 from "../src/components/pages/TestPage2";
 import TestPage3 from "../src/components/pages/TestPage3";
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
         <Wrapper>
-          <Route exact path="/" component={TestPage1} />
+          <Route exact path="/" component={Welcome} />
           <Route exact path="/testpage2" component={TestPage2} />
           <Route exact path="/testpage3" component={TestPage3} />
         </Wrapper>
