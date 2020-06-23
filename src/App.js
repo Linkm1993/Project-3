aa0a7e88d8110a8b67d47ec0f6a4d9dc8f088219
Regina Musovirova <musovirova@yahoo.com>
2020-06-22 22:04:37 -0400

    create toogle component&add it to testpage1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-
 // Components
 import Wrapper from "../src/components/Wrapper";
 import TestPage1 from "../src/components/pages/TestPage1";
 import TestPage2 from "../src/components/pages/TestPage2";
 import TestPage3 from "../src/components/pages/TestPage3";
-
 
 function App() {  
   return (
