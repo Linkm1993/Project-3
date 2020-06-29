b862dcec2ad70b9bd6c53766bf1ad6dfb5fbbc8a
Alex Taylor <altays633@gmail.com>
2020-06-29 19:35:07 -0400

    adding logic to render only welcome page if user is not verified, otherwise render testpage2

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-
 // Components
 import Wrapper from "../src/components/Wrapper";
 import Welcome from "../src/components/pages/Welcome";
 import TestPage2 from "../src/components/pages/TestPage2";
 
+function renderPages() {
+  if (localStorage.getItem("verified") === "true" ) {
+    return (<Wrapper>
+      <Route exact path="/" component={Welcome} />
+      <Route exact path="/testpage2" component={TestPage2} />
+    </Wrapper>)
+  } else {
+    return (<Route exact path = "/" component={Welcome} />)
+  }
+
+}
+
 function App() {  
   return (
     <Router>
       <div className = "App">
-        <Wrapper>
-          <Route exact path="/" component={Welcome} />
-          <Route exact path="/testpage2" component={TestPage2} />
-        </Wrapper>
-        </div>
+        {renderPages()}
+      </div>
     </Router>
   );
 }
