3fb2ff2cee206759f10fa15919ab6506f6b69e17
Alex Taylor <altays633@gmail.com>
2020-06-29 21:39:59 -0400

    Update apiRoutes.js
    removing comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,9 +10,6 @@ const dbName = 'memeImages';
 
 const client = new mongodb.MongoClient(uri);
 
-
-
-
 router.get("/memes", (req,res) => {
     db.MemeTemp.find({})
     .then(memes => res.json(memes))
@@ -26,53 +23,6 @@ router.post("/insert",(req,res) => {
     })
     .then(console.log())
     .catch(err => res.status(422).end())
-
-
-
-
-
-
-
-
-
-
-
-    // client.connect(function(error) {
-    // assert.ifError(error);
-
-    // const db = client.db(dbName);
-
-    // var bucket = new mongodb.GridFSBucket(db);
-
-    // fs.createReadStream('./public/images/surprisedPika.jpg').
-    //     pipe(bucket.openUploadStream('surprisedPika.jpg')).
-    //     on('error', function(error) {
-    //     assert.ifError(error);
-    //     }).
-    //     on('finish', function() {
-    //     console.log('done!');
-    //     process.exit(0);
-    //     });
 });
-
-
-
-
-    // let clientId = "d3579318805c16f";
-    // let data = {
-    //     image: 'https://www.serebii.net/Shiny/SM/025.png',
-    //     type: "url",
-    //     name: "TestPika.png"
-    // };
-    // axios.post('https://api.imgur.com/3/upload', {
-    //     headers: { 
-    //         'Authorization' : 'Client-ID ' + clientId,
-    //     },
-    //     body: data
-    // }).then(function(response) {
-    //     console.log(response);
-    // }).catch(function(error) {
-    //     console.log(error);
-    // });
 
 module.exports = router;
