70b4151285cfc1df5553f45c2a0bac02804c8513
Alex Taylor <altays633@gmail.com>
2020-07-01 22:09:43 -0400

    moving router to renderPages() function to minimize amount of divs needed

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 // Components
-import Wrapper from "../src/components/Wrapper";
 import Welcome from "../src/components/pages/Welcome";
 import TestPage2 from "../src/components/pages/TestPage2";
 
 function renderPages() {
   if (localStorage.getItem("verified") === "true" ) {
-    return (<Wrapper>
-      <Route exact path="/" component={Welcome} />
-      <Route exact path="/testpage2" component={TestPage2} />
-    </Wrapper>)
+    return (
+      <Router>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/testpage2" component={TestPage2} />
+      </Router>
+)
   } else {
-    return (<Route exact path = "/" component={Welcome} />)
+    return (
+      <Router>
+        <Route exact path = "/" component={Welcome} />
+      </Router> 
+    )
   }
 
 }
 
 function App() {  
   return (
-    <Router>
       <div className = "App">
         {renderPages()}
       </div>
-    </Router>
   );
 }
 
