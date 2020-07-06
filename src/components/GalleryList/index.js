39638e984a06912267a56638e64a4d4fe2357874
Alex Taylor <altays633@gmail.com>
2020-07-06 16:25:09 -0400

    adding styling to overflow thumbnails on x axis

--- src/components/GalleryList/index.js
+++ src/components/GalleryList/index.js
@@ -1,19 +1,27 @@
 import React from "react";
-// import { listStyleImage } from "html2canvas/dist/types/css/property-descriptors/list-style-image";
+import "./styles.css"
 
 export function GalleryList({children}) {
-  return <ul>{children}</ul>
+  return (
+    <ul className="gallery-group container">
+      <div className="row text-center">
+      {children}
+      </div>
+    </ul>
+  )
 }
 
 export function GalleryListItem({handleClick, handleMemes, src, title}) {
-
-  return <img src={src} 
-        onLoad={handleMemes} 
-        onClick={handleClick} 
-        alt={title} 
-        style = {{backgroundColor: 'rgba(0,0,0,.4)', 
+  return <div className="col-xs-4">
+        <img src={src} 
+          className="gallery-row"
+          onLoad={handleMemes} 
+          onClick={handleClick} 
+          alt={title} 
+          style = {{backgroundColor: 'rgba(0,0,0,.4)', 
                 padding: '5px', 
                 borderRadius: '5px', 
                 margin: 'auto', 
                 width: '150px'}}/>
+        </div>
 }
