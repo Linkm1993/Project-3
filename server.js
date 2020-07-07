ced830021bac01218fcedd97fe7fff20ef0f2477
Regina Musovirova <musovirova@yahoo.com>
2020-07-07 16:54:48 -0400

    fixed server file

--- server.js
+++ server.js
@@ -4,16 +4,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
-const registrationRoutes = require('./route');
-const config = require('./DB.js');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-    () => { console.log('Database is connected') },
-    err => { console.log('Can not connect to the database' + err) }
-); 
 
 // Serve up static assets (usually on heroku)
 app.use(express.static("build"));
@@ -21,9 +12,6 @@ app.use(express.static("build"));
 // Define middleware
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
-app.use(bodyParser.json());
-app.use("./schema/User", registrationRoutes);
 
 // Connect to the Mongo DB
 mongoose.connect(
